3091a5e753f76f7a67429aa6cd63f374bd6e7a4c
Candela Goncebat <candegonce@hotmail.com>
2015-04-15 02:01:22 -0300

    a ver si lo tenes ahora al servlet userbase

--- src/servlets/UserBase.java
+++ src/servlets/UserBase.java
@@ -23,6 +23,7 @@ public class UserBase extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/LaPlataEnBici/userBase.jsp");
+		//llega estooooooosss¿?¿?
 	}
 
 }
