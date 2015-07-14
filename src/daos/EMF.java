f792e2388b70d6062b2e6331fa71c03f3c7379af
SDLaserna <xerox90913@gmail.com>
2015-07-14 02:47:23 -0300

    aspect version 1.1

--- src/daos/EMF.java
+++ src/daos/EMF.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EMF {
-	private static final EntityManagerFactory em = Persistence.createEntityManagerFactory("jyaa2015_bd2");
+	private static final EntityManagerFactory em = Persistence.createEntityManagerFactory("jyaai");
 			
 	public static EntityManagerFactory getEMF() {
 		System.out.println("se creo la conexion");
