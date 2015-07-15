a084920c70d9335da9a7a4400bd06e6d54b4a05b
Cande <candegonce@hotmail.com>
2015-07-15 02:19:22 -0300

    cambie en el web.xml el nombre del paquete a donde tiene que ir a buscar los rest, tmb agregue unos metodos en el rest para ver si andaban, son como el ejemplo que nos dieron

--- src/rest/Rest.java
+++ src/rest/Rest.java
@@ -1,9 +1,8 @@
 package rest;
 
-
-
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,7 +17,7 @@ import services.LogService;
 import services.LogServiceImp;
 
 
-@Path("/estadisticas")
+@Path("/log")
 public class Rest {
 	@Context
 	UriInfo uriInfo;
@@ -43,8 +42,18 @@ public class Rest {
 	
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public List<Log> getLogs(@PathParam("tipoEntidad") String entidad){
+		
 		return this.getLogService().listarLogs(entidad);
 	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public List<Log> getLogsAsHtml(@PathParam("tipoEntidad") String entidad) {
+		return this.getLogService().listarLogs(entidad);
+	}
+
 	
 }
